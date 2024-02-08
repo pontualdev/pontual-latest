22ec5b22fe3db70f2e094b8095591dc41bf44cd0
IsaquiasMarques <patisaquias2000@gmail.com>
2024-02-08 11:56:08 +0100

    route root path changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'home', loadChildren: () => import('./feature-modules/home/home.module').then(m => m.HomeModule) },
+  { path: '', loadChildren: () => import('./feature-modules/home/home.module').then(m => m.HomeModule) },
   { path: 'posts/:category', loadChildren: () => import('./feature-modules/posts/posts.module').then(m => m.PostsModule) },
   { path: 'post/:slug', loadChildren: () => import('./feature-modules/post/post.module').then(m => m.PostModule) },
   { path: 'about', loadChildren: () => import('./feature-modules/about/about.module').then(m => m.AboutModule) },
   { path: 'terms', loadChildren: () => import('./feature-modules/terms/terms.module').then(m => m.TermsModule) },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
