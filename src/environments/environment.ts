548804c123b987ae8700227140f28128d6837264
IsaquiasMarques <patisaquias2000@gmail.com>
2024-02-13 09:20:14 +0100

    env changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,5 @@
 export const environment = {
     production: false,
-    firebaseConfig: {
-        apiKey: "AIzaSyCgNi_kR8cgSrLRAWCI-fyp2Jiy6IPCAkQ",
-        authDomain: "pontual-news.firebaseapp.com",
-        projectId: "pontual-news",
-        storageBucket: "pontual-news.appspot.com",
-        messagingSenderId: "912140866822",
-        appId: "1:912140866822:web:8ee1298fc9e335707b7a87"
-    },
     backoffice: 'https://pontual-bo.fridoom.com/wp-json/wp/v2',
     domain: 'https://pontual-bo.fridoom.com/',
     newsletter: {
