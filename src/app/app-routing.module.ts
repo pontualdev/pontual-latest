f9c954a27d83407f7d323f361da93a1769fe3656
IsaquiasMarques <patisaquias2000@gmail.com>
2024-02-21 16:26:31 +0100

    resolver commented and vercel.json renamed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PostResolver } from '@core/services/resolvers/post.resolver';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./feature-modules/home/home.module').then(m => m.HomeModule) },
   { path: 'posts/:category', loadChildren: () => import('./feature-modules/posts/posts.module').then(m => m.PostsModule) },
-  { path: 'post/:slug', loadChildren: () => import('./feature-modules/post/post.module').then(m => m.PostModule), resolve: { post: PostResolver } },
+  { path: 'post/:slug', loadChildren: () => import('./feature-modules/post/post.module').then(m => m.PostModule) },
   { path: 'about', loadChildren: () => import('./feature-modules/about/about.module').then(m => m.AboutModule) },
   { path: 'terms', loadChildren: () => import('./feature-modules/terms/terms.module').then(m => m.TermsModule) },
   { path: '**', redirectTo: '/', pathMatch: 'full' },
